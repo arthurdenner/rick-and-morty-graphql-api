6f088681b2ea151df40cdf2ba974e0bc01eaab64
Arthur Denner <arthurdenner7@gmail.com>
2018-03-19 14:07:22 -0300

    feat(resolvers): Adding resolvers for the Character type

--- src/resolvers.js
+++ src/resolvers.js
@@ -22,6 +22,29 @@ const resolvers = {
       return axios.get(url).then(({ data }) => data);
     },
   },
+  Character: {
+    episodes({ episode }) {
+      const urls = episode.map(url => axios.get(url).then(({ data }) => data));
+
+      return axios.all(urls).then(axios.spread((...episodes) => episodes));
+    },
+    location({ location }) {
+      // if the location is 'unknown', we can't query anything
+      if (!location.url) {
+        return location;
+      }
+
+      return axios.get(location.url).then(({ data }) => data);
+    },
+    origin({ origin }) {
+      // same as the location
+      if (!origin.url) {
+        return origin;
+      }
+
+      return axios.get(origin.url).then(({ data }) => data);
+    },
+  },
 };
 
 export default resolvers;
